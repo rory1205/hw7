149b9951cd6c24ee6c7f489799d226548c8d6524
rory <rory1205@gmail.com>
2025-03-29 10:36:04 +0800

    Initial Commit

---
+++ hw7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw7
+//
+//  Created by Rory on 2025/3/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
